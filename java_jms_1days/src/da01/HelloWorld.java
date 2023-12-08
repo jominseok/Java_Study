9c4746fc1218894d9243ce428b4fe73131882b85
jominseok <jom470702@gmail.com>
2023-12-08 14:24:27 +0900

    java_1일차_샘플코드 및 설명

---
+++ java_jms_1days/src/da01/HelloWorld.java
@@ -0,0 +1,16 @@
+package da01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		System.out.println(1);
+		/** 
+		 * @prams args
+		 *메소드를 설명할떼 주석입니다.
+		 */
+		//한줄 주석입니다.
+		
+	}
+
+}
