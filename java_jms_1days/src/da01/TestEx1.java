3edd39bc7a75f142b303c55cb9732ce8b8d5094e
jominseok <jom470702@gmail.com>
2023-12-08 17:40:53 +0900

    1일차_마무리 문제

---
+++ java_jms_1days/src/da01/TestEx1.java
@@ -0,0 +1,24 @@
+package da01;
+
+public class TestEx1 {
+
+	public static void main(String[] args) {
+		Test1();
+		Test2();
+	}
+	
+	public static void Test1() {
+		// 1. 아래 코드에서 잘못된 부분을 찾아 수정하세요
+		// char ch1 = "A";
+		char ch1 = 'A';
+	}
+	
+	public static void Test2() {
+		// 2. 국어, 영어, 수학 성적을 입력하여 평균을 구하려한다. 이때 필요한 변수들을 선언하세요
+		//정수 
+		int Kor, Meth, Eng, sum; 
+		//실수
+		double dKor, dMeth, dEng, dsum;
+		double avg;
+	}
+}
