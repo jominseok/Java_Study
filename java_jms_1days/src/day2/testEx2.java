34c879d13bf40558742363e02c2a92c891141b15
jominseok <jom470702@gmail.com>
2023-12-11 17:32:38 +0900

    홀짝 판별 예제

---
+++ java_jms_1days/src/day2/testEx2.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class testEx2 {
+		//홀짝 판별 예제
+	public static void main(String[] args) {
+		
+		int num = 3;
+		//num를 2로 나누었를때 나머지가 0과 같으면 짝수라고 출력하고,
+		//num가 홀수이면 홀수라고 출력하고, num가 짝수이면 짝수라고 출력하는 코드를 작성하세요
+		// num가 홀수이면 홀수라고 출력하세요
+		
+		if(num % 2 == 0) {
+			System.out.println(num+"는 짝수입니다.");
+		}
+		else if(num%2 == 1) {
+			System.out.println(num+"는 홀수입니다.");
+		}
+		else {
+			System.out.println("정수인지 확인해주세요");
+		}
+
+	}
+
+}
