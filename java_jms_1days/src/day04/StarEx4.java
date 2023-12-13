fe1c499acb1201b30578faf8dc36b86e1cdded8a
jominseok <jom470702@gmail.com>
2023-12-13 12:46:56 +0900

    별예제4 이등변 삼각형 출력

---
+++ java_jms_1days/src/day04/StarEx4.java
@@ -0,0 +1,71 @@
+package day04;
+
+public class StarEx4 {
+
+	public static void main(String[] args) {
+		/*
+		 *     *	i = 1	' ' = 4		* = 1
+		 *    ***	i = 3	' ' = 3		* = 3
+		 *   *****	i = 5	' ' = 2		* = 5
+		 *  *******	i = 7	' ' = 1		* = 7
+		 * *********i = 9	' ' = 0		* = 9
+		 * 					5 - 1		2 * i - 1
+		 * */ 
+		int rows = 5;
+		for(int i = 1; i <= 5; i++) {
+			// ' '출력 5-1개
+			for (int j = 1; j <= rows - i; j++) {
+				System.out.print(' ');
+			}
+			// *출력 2*i-1개
+			for(int j = 1; j <= 2*i-1; j++) {
+				System.out.print("*");
+			}
+			// 엔터
+			System.out.println();
+		}
+			/*
+			 *     *	 i = 1	' ' = 4		* = 1
+			 *    ** *	 i = 2	' ' = 3		* = 3
+			 *   *** **	 i = 3	' ' = 2		* = 5
+			 *  **** *** i = 4	' ' = 1		* = 7
+			 * ***** ****i = 5	' ' = 0		* = 9
+			 * 					5 - 1		2 * i - 1
+			 * */ 
+		for(int i = 1; i>5; i++) {
+			//' '을 5-i개 출력
+			for(int j = 1; j < rows-i; j++) {
+				System.out.print(' ');
+			}
+			// *을 i개 출력
+			for(int j = 1; j<=i; j++) {
+				System.out.print("*");
+			}
+			//*을 i-1개 출력
+			for(int j =1; j <= i; j++) {
+				System.out.print("*");
+			}
+			// 엔터
+			System.out.println();
+		}
+		/*
+		 * *****
+		 *  ****
+		 *   ***
+		 *    **
+		 *     *
+		 *     
+		 *     *
+		 *    ***
+		 *   *****
+		 *  *******
+		 * *********
+		 * *********
+		 *  *******
+		 *   *****
+		 *    ***
+		 *     *
+		 * */
+	}
+
+}
