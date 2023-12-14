803b181a14c43da501d6c892a9713f4adbcc4087
jominseok <jom470702@gmail.com>
2023-12-14 13:44:33 +0900

    day02 성인 판별 예제

---
+++ java_jms_1days/src/day02/TestEx4.java
@@ -0,0 +1,22 @@
+package day02;
+
+import java.util.Scanner;
+
+public class TestEx4 {
+
+	public static void main(String[] args) {
+		//성인 판별 예제
+		//나이를 입력받아 나이가 20세 이상이면 성인을 출력하고
+		//20세 미만이면 미성년자 출력하는 코드를 작성하시오
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("나이를 입력해주세요 : ");
+		
+		int old = scan.nextInt();
+		
+		System.out.println(old<20? "미성년자" : "성인");
+		
+		scan.close();
+	}
+
+}
