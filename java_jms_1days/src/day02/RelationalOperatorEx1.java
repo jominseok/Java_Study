d1e20a024030cb2ec4d50f59a26e04a682803bfc
jominseok <jom470702@gmail.com>
2023-12-14 13:43:19 +0900

    day02 비교 연산자 예제

---
+++ java_jms_1days/src/day02/RelationalOperatorEx1.java
@@ -0,0 +1,19 @@
+package day02;
+
+public class RelationalOperatorEx1 {
+	
+	//비교 연산자 예제
+	public static void main(String[] args) {
+		
+		int num1 = 1, num2 = 2;
+		
+		System.out.println(num1+" >  : " + num2 +  (num1 > num2));
+		System.out.println(num1+" >=  : " + num2 + (num1 >= num2));
+		System.out.println(num1+" <  : " + num2 + (num1 < num2));
+		System.out.println(num1+" <=  : " + num2 + (num1 <= num2));
+		System.out.println(num1+" ==  : " + num2 + (num1 == num2));
+		System.out.println(num1+" != : " + num2 + (num1 != num2));
+		
+	}
+
+}
