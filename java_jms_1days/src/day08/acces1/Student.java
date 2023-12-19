9ca7168187496a01998d6d2c0e83aceb3fd776df
jominseok <jom470702@gmail.com>
2023-12-19 17:39:21 +0900

    자바8일차 접근제어자 및 정보은닉 예제

--- java_jms_1days/src/day08/acces1/Student.java
+++ java_jms_1days/src/day08/acces1/Student.java
@@ -5,4 +5,31 @@ public class Student {
 	int calssNum; //접근 제어자가 default
 	public int num;
 	private String name;
+	
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public int getCalssNum() {
+		return calssNum;
+	}
+	public void setCalssNum(int calssNum) {
+		this.calssNum = calssNum;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
--- java_jms_1days/src/day08/acces2/AccessModifierEx2.java
+++ java_jms_1days/src/day08/acces2/AccessModifierEx2.java
@@ -7,11 +7,9 @@ public class AccessModifierEx2 {
 	public static void main(String[] args) {
 		
 		Student std = new Student();
-		//std.grade = 1;// grade의 접근 제어자가 private이어서 같은 패키지에 있는 클래스에서 사용X
-					 // grade의 패키지가 아닌 다른 패키지에서 사용 x
-		//std.calssNum = 1; // classNum의 접근 제어자가 defualt이어서 같은 패키지에 있는 클래스에서 사용 O
-						  // classNum의 접근 제어자가 defualt이어서 다른 패키지에서 사용 X
-		std.num = 1; // num의 접근 제어자가 public이어서 사용 O
+		//std.grade = 1;//grade의 접근제어자가 private이어서 다른 패키지에 있는 클래스에서 사용 X
+		//std.classNum = 1; //classNum의 접근제어자가 default이어서 다른 패키지에 있는 클래스에서 사용 X
+		std.num = 1;//num의 접근제어자가 public이어서 사용 O
 		
 	}
 
