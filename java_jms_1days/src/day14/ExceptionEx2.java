a2f4665fb794765b301caeab5475b5f1e3523394
jominseok <jom470702@gmail.com>
2023-12-28 12:49:12 +0900

    자바 14일차 throw/throws예제

---
+++ java_jms_1days/src/day14/ExceptionEx2.java
@@ -0,0 +1,24 @@
+package day14;
+
+public class ExceptionEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test();
+	}
+	
+	public static void test() {
+		int num1 = 1, num2 = 0;
+		try {
+			System.out.println(num1/num2);
+		}catch(ArithmeticException e) {
+			System.out.println("0으로 나눌 수 없습니다.");
+			return;
+		}finally {
+			// 위에서 예외가 발생해서 처리하고, retrun을 통해 빠져나가도 실행
+			System.out.println("finally 실행");
+		}
+		//위에서 return으로 빠져나가면 실행되지 않음
+		System.out.println("메서드 종료");
+	}
+}
---
+++ java_jms_1days/src/day14/ExceptionEx4.java
@@ -0,0 +1,50 @@
+package day14;
+
+import lombok.Data;
+
+public class ExceptionEx4 {
+
+	//사용자가 만든 예외처리문 사용 방법
+	public static void main(String[] args) {
+		
+		try {
+			test();
+		} catch (MyException e) {
+			e.print();
+			e.printStackTrace();
+		}
+		try {
+			System.out.println(1/0);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();// 예외가 발생한 것들을 차례대로 잡아줌
+		}
+		
+		System.out.println("종료");
+	}
+	
+	public static void test() throws MyException{
+		throw new MyException("안녕하세요", "무조건 예외 발생");
+	}
+
+}
+
+
+//사용자가 만드는 예외처리문 
+@Data
+class MyException extends Exception{
+	
+	private String title;
+	
+	public MyException() {}
+	
+	public MyException(String title, String message) {
+		super(message);
+		this.title = title;
+	}
+	
+	public void print() {
+		System.out.println(title);
+		System.out.println(getMessage());
+	}
+}
