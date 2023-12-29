e552786c71cc8b637d12a1d254145aa137fad1e0
jominseok <jom470702@gmail.com>
2023-12-29 17:42:06 +0900

    자바 15일차 Map예제

--- java_jms_1days/src/day15/MapEx2.java
+++ java_jms_1days/src/day15/MapEx2.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class MapEx2 {
+	
 	/* 회원을 관리하는 프로그램을 작성하세요.
 	 * 메뉴
 	 * 1. 회원가입
@@ -40,44 +41,54 @@ public class MapEx2 {
 		
 	}
 	
-	private static void runMenu(int menu) {
-		
-		switch (menu) {
+	private static void runMenu(int menu) {//throws를 생략해도 되는가? RuntimeException일 경우 <---저기에 throws를 생략 가능하다 
+		switch (menu) {	         		   //InputMismatchException의 경우 RuntimeException의 자식 클래스이기 때문에 생략 가능
 		case 1: {
 			//회원 가입
-			System.out.print("아이디를 입력해주세요 : ");
-			String id = scan.next();
-			System.out.print("비밀번호를 입력해 주세요 : ");
-			String pw = scan.next();
-			if(map.containsKey(id)) {
-				System.out.println("중복된 아이디입니다.");
-				return;
-			}
-			map.put(id, pw);
-			System.out.println("회원가입에 성공하였습니다.");
+			insertMember();
 			break;
 		}
 		case 2: {
 			//회원 검색
-			System.out.print("회원 아이디를 입력해주세요 : ");
-			String member = scan.next();
-			for(String tmp : map.keySet()) {
-				if(tmp.equals(member)) {
-					System.out.println(tmp + " 회원이 검색되었습니다.");
-					return;
-				}
-			}
-			System.out.println("검색된 회원이 없습니다.");
+			serchMember();
 			break;
 		}
 		case 3: {
 			System.out.println("종료");
 			break;
 		}
-	
 		default:
 			throw new InputMismatchException();
 		}
+	}
+
+	private static void serchMember() {
+		System.out.print("회원 아이디를 입력해주세요 : ");
+		String id = scan.next();
+		
+		//아이디, 비번을 출력
+		//비번을 가지고 옴
+		String pw = map.get(id);
+		
+		//가져온 비번이 null이면 없는 회원이라고 출력
+		if(pw == null) {
+			System.out.println("등록되지 않은 아이디입니다.");
+		}
+		//아니면 id, pw를 출력
+		System.out.println("아이디 : " + id + "비밀번호 : " + pw);
+	}
+
+	private static void insertMember() {
+		System.out.print("아이디를 입력해주세요 : ");
+		String id = scan.next();
+		if(map.containsKey(id)) {//if(map.get(id) != null)
+			System.out.println("중복된 아이디입니다.");
+			return;
+		}
+		System.out.print("비밀번호를 입력해 주세요 : ");
+		String pw = scan.next();
+		map.put(id, pw);
+		System.out.println("회원가입에 성공하였습니다.");
 	}
 
 	private static void printMenu() {
