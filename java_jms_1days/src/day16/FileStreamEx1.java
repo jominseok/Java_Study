7240a047390699ed75155085a2dfffa01a9632d0
jominseok <jom470702@gmail.com>
2024-01-02 13:56:45 +0900

    자바 16일차 FileStream예제

--- java_jms_1days/src/day16/FileStreamEx1.java
+++ java_jms_1days/src/day16/FileStreamEx1.java
@@ -10,15 +10,14 @@ public class FileStreamEx1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		FileInputStream fis1 = null;
-		FileOutputStream fos1 =null;
-		
 		String fileName = "src/day16/fileEx3";
 		
-		try {
-			fos1 = new FileOutputStream(fileName);
-			fis1 = new FileInputStream(fileName);
-			
+		//try resource를 이용한 예제로
+		//파일을 열고나서 따로 닫지 않아도 try문이 종료되면 자동으로 닫아짐
+		try( FileInputStream fis1 = new FileInputStream(fileName);
+				FileOutputStream fos1 = new FileOutputStream(fileName);	
+				) {
+	
 			String str = "hello";
 			
 			for(int i = 0; i <str.length(); i++) {
@@ -34,16 +33,8 @@ public class FileStreamEx1 {
 			System.out.println("파일을 찾을 수 없습니다.");
 		}catch (Exception e) {
 			System.out.println("파일 작업 중 예외 발생");
-		} finally {
-			try {
-				fos1.close();
-				fis1.close();
-			} catch (IOException e2) {
-				System.out.println("파일을 닫을 수 없습니다.");
-			}
 		}
-		
-		
+
 	}
 
 }
