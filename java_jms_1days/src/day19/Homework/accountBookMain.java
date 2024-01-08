adefcf9fe7f1cff208dcd4d86cdaf4f248e086cf
jominseok <jom470702@gmail.com>
2024-01-08 17:50:13 +0900

    자바 19일차

---
+++ java_jms_1days/src/day19/Homework/accountBookMain.java
@@ -0,0 +1,21 @@
+package day19.Homework;
+
+public class accountBookMain {
+	
+	
+	/* 2023년 가계부 프로그램
+	 * 2023년 1~12월 까지의 가계부 프로그램을 만든다.
+	 * 
+	 *  기능
+	 *  1~12월 선택 기능
+	 *  각 월에 맞는 일 선택 후 지출, 수입, 메모 기능
+	 *  통계기능(1~12월 까지의 수입 및 합계, 1~12월 까지의 지출 및 합계)
+	 *  조회 기능 (월과 일을 선택하면 
+	 *  
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
