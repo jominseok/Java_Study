70365bf51c8dd1281dba5e5896adc286fd5eab2e
jominseok <jom470702@gmail.com>
2024-02-13 16:21:47 +0900

    데이터베이스 7일차 트랜잭션

---
+++ database/sql/20_트랜잭션.sql
@@ -0,0 +1,12 @@
+start transaction; #트랜잭션 시작
+# begin/begin work
+use shoppingmall;
+
+# FOO002 가격을 2000원으로 수정
+update product set pr_price = 2000 where pr_code = 'FOO002';
+
+savepoint a;
+update product set pr_price = 1000 where pr_code = 'FOO002';
+rollback to a;
+commit;
+select * from product;
