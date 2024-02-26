094abe164dd91ef4f3e6ea239cd53cd231ed64c0
jominseok <jom470702@gmail.com>
2024-02-26 10:08:03 +0900

    서블릿 3일차 : 서블릿 복습 - 프로젝트 생성 및 메인 화면 연결 완료

--- servlet_project2/src/main/java/kr/kh/app/controller/MainServelt.java
+++ servlet_project2/src/main/java/kr/kh/app/controller/MainServelt.java
@@ -7,25 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @WebServlet("/")
 public class MainServelt extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	public MainServelt() {
-		
-	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// jsp파일에서 받은 데이터 값을 받는 방법 예제
-		String data = request.getParameter("data");
-		System.out.println("전송받은 데이터 : " + data);
-		String id = request.getParameter("id");
-		System.out.println("전송받은 ID : " + id);
-		//화면으로 나이를 전송
-		int age = 30;
-		request.setAttribute("age", age);//서버에서 화면으로 age을 전송
 		request.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(request, response);
 	}
 
-	
 }
