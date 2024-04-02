f1caee3bbaaf583615b4f2d4745585e8d52dc8df
jominseok <jom470702@gmail.com>
2024-04-02 09:16:30 +0900

    .

--- spring/sql/community_member_state.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE  IF NOT EXISTS `community` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `community`;
-DROP TABLE IF EXISTS `member_state`;
-CREATE TABLE `member_state` (
-  `ms_state` varchar(10) NOT NULL,
-  PRIMARY KEY (`ms_state`)
-);
